33525b29e646442e5f1b6ed1037991a9f7f81d07
Amy Chan <chamy09@gmail.com>
2019-01-06 01:42:44 -0600

    CSS fix and add personal projects

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -6,56 +6,83 @@
 
     <div class="projects">
       <div class="project">
-        <h3><a href="https://www.toysrus.dk" target="_blank">Toys R Us (Nordic Countries)</a> /
-            <a href="https://www.br.dk" target="_blank">BR Toys</a></h3>
+        <h3>
+          <i class="fas fa-angle-right"></i>
+          <a href="https://www.toysrus.dk" target="_blank">Toys R Us (Nordic Countries)</a> /
+          <a href="https://www.br.dk" target="_blank">BR Toys</a>
+        </h3>
         <p>Front End Development, Back End Development</p>
       </div>
 
       <div class="project">
-        <h3><a href="http://www.elfcosmetics.com" target="_blank">ELF Cosmetics</a></h3>
+        <h3>
+          <i class="fas fa-angle-right"></i>
+          <a href="http://www.elfcosmetics.com" target="_blank">ELF Cosmetics</a>
+        </h3>
         <p>Front End Development, Back End Development</p>
       </div>
 
       <div class="project">
-        <h3><a href="https://www.nydj.com" target="_blank">NYDJ</a></h3>
+        <h3>
+          <i class="fas fa-angle-right"></i>
+          <a href="https://www.nydj.com" target="_blank">NYDJ</a>
+        </h3>
         <p>Front End Development, Back End Development</p>
       </div>
 
       <div class="project">
-        <h3><a href="https://www.gymboree.com" target="_blank">Gymboree</a> /
-            <a href="https://www.crazy8.com" target="_blank">Crazy 8</a> /
-            <a href="https://www.janieandjack.com/" target="_blank">Janie and Jack</a></h3>
+        <h3><i class="fas fa-angle-right"></i>
+          <a href="https://www.gymboree.com" target="_blank">Gymboree</a> /
+          <a href="https://www.crazy8.com" target="_blank">Crazy 8</a> /
+          <a href="https://www.janieandjack.com/" target="_blank">Janie and Jack</a>
+        </h3>
         <p>Front End Development, Back End Development</p>
       </div>
 
       <div class="project">
-        <h3><a href="http://jieyisushi.com" target="_blank">Jieyi Sushi</a></h3>
+        <h3>
+          <i class="fas fa-angle-right"></i>
+          <a href="http://jieyisushi.com" target="_blank">Jieyi Sushi</a>
+        </h3>
         <p>Design, Front End Development</p>
-        <p>Tools Used: Materialize, javascript, jQuery, PHP, Photoshop</p>
+        <p>Tools Used: Materialize, Javascript, jQuery, PHP, Photoshop</p>
       </div>
 
       <div class="project">
-        <h3><a href="http://www.paramountoakpark.com" target="_blank">Paramount of Oak Park</a></h3>
+        <h3><i class="fas fa-angle-right"></i>
+          <a href="http://www.paramountoakpark.com" target="_blank">Paramount of Oak Park</a>
+        </h3>
         <p>Front End Development, Back End Development</p>
-        <p>Tools Used: Ruby on Rails, javascript, Bootstrap</p>
+        <p>Tools Used: Ruby on Rails, Javascript, Bootstrap</p>
       </div>
     </div>
 
-    <!-- <div class="projects">
+    <div class="projects">
       <h2>Personal Projects</h2>
 
       <div class="project">
-        <h3>ColorCrush</h3>
+        <h3>
+          <i class="fas fa-angle-right"></i>
+          <a href="http://color-crush.herokuapp.com" target="_blank">ColorCrush</a>
+        </h3>
+        <p>Tools Used: Ruby on Rails, Javascript, AngularJS, Foundation, Colorthief.js</p>
+        <p><a href="https://github.com/amyology/colorcrush" target="_blank">See code</a></p>
       </div>
 
       <div class="project">
-        <h3>Analytics Dashboard</h3>
+        <h3><i class="fas fa-angle-right"></i>
+          <a href="http://prod-spark-start.herokuapp.com" target="_blank">Spark Start</a>
+        </h3>
+        <p>Tools Used: Ruby on Rails, Vue.js, Bootstrap</p>
       </div>
 
       <div class="project">
-        <h3>Spark Start</h3>
+        <h3><i class="fas fa-angle-right"></i>
+          <a href="http://tde-analytics.herokuapp.com" target="_blank">Analytics Dashboard</a>
+        </h3>
+        <p>Tools Used: Rails, AngularJS, Materialize</p>
       </div>
-    </div> -->
+    </div>
 
   </div>
 </template>
@@ -67,5 +94,14 @@
     &:first-of-type {
       margin-top: 0;
     }
+  }
+
+  h2 {
+    text-decoration: underline;
+  }
+
+  i:after {
+    content: '';
+    margin-right: 5px;
   }
 </style>
