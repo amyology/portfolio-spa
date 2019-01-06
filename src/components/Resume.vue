873b2942b0c671a26a3abc8ec41051f8b6f4adaf
Amy Chan <chamy09@gmail.com>
2019-01-06 12:46:40 -0600

    Move 404.html

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -3,7 +3,7 @@
     <h1>Resume</h1>
     <p>
       <i class="fas fa-angle-double-down"></i>&nbsp;
-      <a href="/AmyChanResume.pdf">Download</a> a copy of my resume.
+      <a href="/AmyChanResume.pdf" target="_blank">Download</a> a copy of my resume.
     </p>
 
     <h2>Skills</h2>
